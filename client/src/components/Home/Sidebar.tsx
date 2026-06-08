0503cd3ce430684cec180e6ec5e8c1af4478e394
Your Name <akashmondkar03@gmail.com>
2026-06-08 20:54:52 +0530

    fixing errors

---
+++ client/src/components/Home/Sidebar.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const Sidebar = () => {
+
+    const[isMobileMenuOpen,setIsMobileMenuOpen ]= useState(false)
+    
+  return (
+    <div className='flex h-screen bg-slate-50'>
+        {/* Mobile Overlay */}
+         
+
+
+      <h1>sidebar</h1>
+    </div>
+  );
+}
+
+export default Sidebar;
