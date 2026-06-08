f2a7521c20ef6e031b13318519cf45b3a6dbe5cc
Your Name <akashmondkar03@gmail.com>
2026-06-08 21:04:34 +0530

    adding mobile menu

--- client/src/components/Home/Sidebar.tsx
+++ client/src/components/Home/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const Sidebar = () => {
 
@@ -7,10 +8,27 @@ const Sidebar = () => {
   return (
     <div className='flex h-screen bg-slate-50'>
         {/* Mobile Overlay */}
+
+        {isMobileMenuOpen && <div className='fixed insert-0 bg-slate-300/50 z-40 md:hidden' 
+        onClick={()=> setIsMobileMenuOpen(false)}
+        />}
          
+      <Sidebar isOpen={isMobileMenuOpen} setIsopen={setIsMobileMenuOpen}/>
+       
 
+       <div className='flex-1 flex flex-col overflow-hidden'>
+         {/* Top Bar */}
 
-      <h1>sidebar</h1>
+         <header>
+
+         </header>
+         <main className='flex-1 overflow-auto p-4 sm:p-8 xl:p-12'>
+
+          <Outlet/>
+         </main>
+
+       </div>
+     
     </div>
   );
 }
